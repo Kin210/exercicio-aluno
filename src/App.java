7f7b0dad8640f508fe60a6a4b37008aa1b8018d7
Felipe Luiz <felipe3003gomes@gmail.com>
2026-02-28 15:39:25 -0300

    criado o calcula da nota do aluna

---
+++ src/App.java
@@ -0,0 +1,25 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+                
+    public static void main(String[] args) throws Exception {
+    
+      Locale.setDefault(Locale.US);
+      Scanner sc = new Scanner(System.in);
+     
+      Double n1, n2, n3,n4, n5;
+
+      System.out.println("Notas   ");
+      System.out.println("Primeira nota:");
+      n1 = sc.nextDouble();
+      
+
+      
+
+
+      sc.close();
+     }
+   }
+
---
+++ src/application/Programas.java
@@ -0,0 +1,54 @@
+package application;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+
+
+   public class Programas {
+   public static void main(String[] args) {
+
+
+      Locale.setDefault(Locale.US);
+      Scanner sc = new Scanner(System.in);
+
+
+       System.out.println("Notas do Aluno:");
+
+        double n1, n2, n3, n4, n5;
+
+          System.out.print("1° Bimeste: ");
+           n1 = sc.nextDouble();
+          System.out.print("2° Bimeste: ");
+           n2 = sc.nextDouble();
+          System.out.print("3° Bimeste: ");
+           n3 = sc.nextDouble();
+          System.out.print("4° Bimeste: ");
+           n4 = sc.nextDouble();
+          System.out.print("Nota Final: ");
+           n5 = sc.nextDouble();
+
+         double media = n1 + n2 + n3 + n4 + n5 / 5;
+
+         System.out.printf("Media do aluno: %.2f%n", media);
+         
+         if (media >= 7.00) {
+            System.out.println("Aprovado");
+         }else 
+            System.out.println("Reprovado");
+
+
+
+        
+         
+
+      
+      sc.close();
+    
+    
+   }
+  
+}
